47f52b15312a2e3ab951e2ee2ec6951ce50865a5
ftmmchan <67224648+ftmmchan@users.noreply.github.com>
2022-07-15 07:11:31 +0900

    test

--- Template_3/uninstall.sh
+++ Template_3/uninstall.sh
@@ -6,7 +6,7 @@
 
 ROOT_UID=0
 THEME_DIR="/usr/share/grub/themes"
-THEME_NAME="Template_2"
+THEME_NAME="Template_3"
 
 MAX_DELAY=20                                        # max delay for user to enter root password
 
@@ -44,7 +44,7 @@ prompt () {
 }
 
 # Welcome message
-  prompt -s "\n\t   **********************************************************\n\t   * Template_2 Bootloader Theme *\n\t   **********************************************************\n
+  prompt -s "\n\t   **********************************************************\n\t   * Template_3 Bootloader Theme *\n\t   **********************************************************\n
                                   GRUB Theme
 
                                       by
